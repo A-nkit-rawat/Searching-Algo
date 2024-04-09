b33d150bd1dfeebdf8550878567a0df4e2e49662
AnkitRwt <23mca0046@gmail.com>
2024-04-09 23:06:55 +0530

    bubble Sort Alogrithm

---
+++ Bubble_Sort.java
@@ -0,0 +1,25 @@
+// time complexity O(n^2)
+// Optimize version of bubble sort ( best case time complexity is O(n) )
+
+class Bubble_Sort{
+
+    public static void bubbleSort(int [] ele){
+        boolean flag=true;
+        for(int i=0;i<ele.length-1;i++){
+            flag=true;
+            for(int j=0;j<(ele.length-1)-i;j++){
+                if(ele[j]>ele[j+1]){
+
+                    //swapping
+                    int temp=ele[j+1];
+                    ele[j+1]=ele[j];
+                    ele[j]=temp;
+                    flag=false;
+                }
+
+                //if no element swap by inner loop, then break
+                if(flag==true) break;
+            }
+        }
+    }
+}
